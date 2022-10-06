3440f20606c360c955c06951d97adf4505bb4223
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-10-06 17:01:09 +0530

    Add files via upload

---
+++ STATEMENTS IN JAVA ACTIVITY 5(1)/Prime1.java
@@ -0,0 +1,17 @@
+package pro1;
+
+public class Prime1 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			for(int i=1;i<=150;i++) {
+					
+				for(int j=2;j<=i;j++) {
+					if(i%j!=0) 
+						System.out.print(i+" ");
+					break;
+					
+				}
+			}
+		}
+}
